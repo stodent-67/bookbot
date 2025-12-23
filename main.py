1ad1a8c47207333ef320706ec4bed0264da536d8
stodent-67 <thomas.gavin.25@gmail.com>
2025-12-23 19:52:24 +1100

    L2 Arguments

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from stats import get_book_text, get_num_words, get_num_char, sort_on, chars_dict_to_sorted_list
-path_to_file = "books/frankenstein.txt"
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+path_to_file = sys.argv[1]
+
 def main():
     text = get_book_text(path_to_file)
     num_words = get_num_words(text)
@@ -13,6 +20,5 @@ def main():
     for entry in sorted_dict:
         print(f"{entry["char"]}: {entry["num"]}")
     print("============= END ===============")
-
 
 main()
