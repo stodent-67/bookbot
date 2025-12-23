12a8309c0c933d9c505a6b7cd57ded333b1a9947
stodent-67 <thomas.gavin.25@gmail.com>
2025-12-23 14:26:48 +1100

    fixed stats module 1

--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+
 def get_num_words():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
