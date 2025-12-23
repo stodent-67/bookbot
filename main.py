b2d8e27eb4bab85e3df8fb0d09fc433b33e88fbe
stodent-67 <thomas.gavin.25@gmail.com>
2025-12-23 16:40:34 +1100

    usdnfjosfnosdf

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from stats import get_num_words
+from stats import get_book_text, get_num_words, get_num_char, sort_on, chars_dict_to_sorted_list
+path_to_file = "books/frankenstein.txt"
 def main():
-    get_num_words()
+    text = get_book_text(path_to_file)
+    get_num_char(text)
 
 main()
--- stats.py
+++ stats.py
@@ -1,10 +1,32 @@
+path_to_file = "books/frankenstein.txt"
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
+     
+text = get_book_text(path_to_file)
 
 def get_num_words():
-    path_to_file = "books/frankenstein.txt"
-    text = get_book_text(path_to_file)
     num_words = len(text.split())
     print(f"Found {num_words} total words")
+
+def get_num_char(text):
+    num_char = {}
+    for ch in text:
+        ch = str.lower(ch)
+        if ch in num_char:
+            num_char[ch] = num_char[ch] + 1
+        else:
+            num_char[ch] = 1
+    return num_char
+
+def sort_on(items):
+    return items["num"]
+
+def chars_dict_to_sorted_list(num_char):
+    new_list = []
+    for key in num_char:
+        new_list.append({"char": key, "num": num_char[key]})
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
